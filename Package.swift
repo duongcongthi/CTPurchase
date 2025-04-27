d72b29c1b031659e65f18aba63f0e92c6eaf3c1a
DUONG THI <duongcongthi123@gmail.com>
2025-04-28 01:46:31 +0700

    fix code

--- Package.swift
+++ Package.swift
@@ -1,20 +1,24 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "CTPurchase",
     platforms: [
-        .iOS(.v15) // Chỉ hỗ trợ iOS 15+
+        .iOS(.v15)
     ],
     products: [
         .library(
             name: "CTPurchase",
-            targets: ["CTPurchase"]),
+            targets: ["CTPurchase"]
+        ),
     ],
     targets: [
         .target(
-            name: "CTPurchase"),
+            name: "CTPurchase",
+            path: "Sources/CTPurchase"
+        ),
     ]
 )
